62dc7afbfd4a480a8f86206a5bdf6271575ee5d3
pauljoda <pauldavis101@gmail.com>
2025-07-24 18:43:22 -0500

    Initial Commit

---
+++ Overlap/OverlapApp.swift
@@ -0,0 +1,32 @@
+//
+//  OverlapApp.swift
+//  Overlap
+//
+//  Created by Paul Davis on 7/24/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OverlapApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
